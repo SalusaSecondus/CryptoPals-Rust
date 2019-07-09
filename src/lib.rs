e98adf92ac9a4c775a2816be35354d6d8980c58a
Greg Rubin <rubin@amazon.com>
2019-07-09 15:13:40 -0700

    Set 1, Prob 3

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,53 @@
+mod set1;
+
+#[macro_use]
+extern crate lazy_static;
+
 pub use failure::Error;
 use failure::err_msg;
+use std::collections::HashMap;
 
 pub type Result<T>  = std::result::Result<T, Error>;
 
+lazy_static! {
+    static ref MONOGRAM_FREQUENCIES : HashMap<&'static str, f32> = {
+        let mut m = HashMap::new();
+        m.insert(" ", 0.1217);
+        m.insert("a", 0.0609);
+        m.insert("b", 0.0105);
+        m.insert("c", 0.0284);
+        m.insert("d", 0.0292);
+        m.insert("e", 0.1136);
+        m.insert("f", 0.0179);
+        m.insert("g", 0.0138);
+        m.insert("h", 0.0341);
+        m.insert("i", 0.0544);
+        m.insert("j", 0.0024);
+        m.insert("k", 0.0041);
+        m.insert("l", 0.0292);
+        m.insert("m", 0.0276);
+        m.insert("n", 0.0544);
+        m.insert("o", 0.0600);
+        m.insert("p", 0.0195);
+        m.insert("q", 0.0024);
+        m.insert("r", 0.0495);
+        m.insert("s", 0.0568);
+        m.insert("t", 0.0803);
+        m.insert("u", 0.0243);
+        m.insert("v", 0.0097);
+        m.insert("w", 0.0138);
+        m.insert("x", 0.0024);
+        m.insert("y", 0.0130);
+        m.insert("z", 0.0003);
+        m
+    };
+}
+
+
 pub fn hex_to_bin(hex : &str) -> Result<Vec<u8>> {
     let mut result : Vec<u8> = Vec::new();
-    for i in (0..hex.len()).step_by(2) {
-        let byte = u8::from_str_radix(&hex[i..i+2], 16)?;
+    for hex_byte in string_chunk(hex, 2) {
+        let byte = u8::from_str_radix(hex_byte, 16)?;
         result.push(byte);
     }
 
@@ -41,14 +82,68 @@ pub fn xor(v1 : &Vec<u8>, v2 : &Vec<u8>) -> Result<Vec<u8>> {
     }
 }
 
+pub fn string_chunk(string : &str, size : usize) -> Vec<&str> {
+    let mut result : Vec<&str> = Vec::new();
+    let mut indices = string.char_indices();
+
+    let mut start = indices.next().unwrap().0;
+    loop {
+        let mut end = Option::None;
+        for _x in 0..size {
+            end = indices.next();
+        }
+        if end.is_some() {
+            let idx_end = end.unwrap().0;
+            let tmp = &string[start..idx_end];
+            result.push(tmp);
+            start = idx_end;
+        } else {
+            let tmp = &string[start..];
+            result.push(tmp);
+            break;
+        }
+    }
+    result
+}
+
+pub fn monograph_score(string : &str) -> f32 {
+    let mut total : f32 = 0.0;
+    let mut cnts : HashMap<&str, f32> = HashMap::new();
+    let mut result = 0.0;
+
+    let lowercase = string.to_lowercase();
+    for mono in string_chunk(&lowercase, 1) {
+        let mono_cnt = cnts.entry(mono).or_insert(0.0);
+        *mono_cnt += 1.0;
+        total += 1.0;
+    }
+
+    for (key, val) in MONOGRAM_FREQUENCIES.iter() {
+        let freq = cnts.get(key).unwrap_or(&0.0) / total;
+        let diff = freq - val;
+        let sq_diff = diff * diff;
+        result += sq_diff;
+    }
+    
+    result
+}
+
+pub fn bytes_to_string(bytes : &Vec<u8>) -> String {
+    String::from_utf8_lossy(bytes).to_string()
+    // let mut result = String::new();
+    // for b in bytes {
+    //     // if *b < 128 {
+    //         result.push(*b as char)
+    //     // } else {
+    //     //     result.push('\u{FFFD}');
+    //     // }
+    // }
+    // result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 
     #[test]
     fn set1_prob1() {
--- src/set1.rs
+++ src/set1.rs
@@ -1,2 +1,38 @@
 // https://cryptopals.com/sets/1
 
+use super::*;
+
+fn single_byte_xor(bin : &Vec<u8>, twiddle : u8) -> Result<Vec<u8>> {
+    let mut result = bin.clone();
+    for val in result.iter_mut() {
+        *val ^= twiddle;
+    }
+    Ok(result)
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    pub fn set1_prob3() {
+        let ciphertext = hex_to_bin("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736").unwrap();
+
+        let mut low_score = 1000000.0;
+        let mut low_twiddle = 0;
+        for twiddle in 0..255 {
+            let raw = single_byte_xor(&ciphertext, twiddle).unwrap();
+            let guess = bytes_to_string(&raw);
+            let score = monograph_score(&guess);
+            if score < low_score {
+                low_score = score;
+                low_twiddle = twiddle;
+            }
+            println!("{} {}: {}", twiddle, monograph_score(&guess), guess);
+        }
+        let raw = single_byte_xor(&ciphertext, low_twiddle).unwrap();
+        let guess = bytes_to_string(&raw);
+        println!("{} {}: {}", low_twiddle, monograph_score(&guess), guess);
+        
+    }
+}
