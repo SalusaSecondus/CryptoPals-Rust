d1fedf52e89c2408c1796e32ff4fa1d2f0975e6b
SalusaSecondus <salusa@salusa.dev>
2021-04-10 15:14:57 -0700

    Challenge 36

--- src/srp.rs
+++ src/srp.rs
@@ -4,13 +4,12 @@ use anyhow::{Context, Result};
 use lazy_static::lazy_static;
 use num_bigint::BigUint;
 
-use crate::digest::{Digest, Hmac, Sha1};
+use crate::digest::{Digest, Hmac, Sha256};
 use crate::math::{mod_exp, rand_bigint};
 
 lazy_static! {
     static ref NIST_PRIME: BigUint =  BigUint::parse_bytes(b"ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff", 16).unwrap();
-    // TODO: Make this SHA-256
-    pub static ref SRP_STANDARD: SrpParams<Sha1>  = SrpParams {
+    pub static ref SRP_STANDARD: SrpParams<Sha256>  = SrpParams {
         n: NIST_PRIME.clone(),
         g: 2u32.into(),
         k: 3u32.into(),
