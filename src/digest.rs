5783db9ba27079e1872d1f2214bb33b610e7e8a9
SalusaSecondus <salusa@salusa.dev>
2021-04-10 15:14:43 -0700

    SHA256

--- src/digest.rs
+++ src/digest.rs
@@ -160,6 +160,159 @@ impl Digest for Sha1 {
 }
 
 #[derive(Clone)]
+pub struct Sha256 {
+    h: [u32; 8],
+    ml: u64,
+    buffer: Vec<u8>,
+}
+
+impl Default for Sha256 {
+    fn default() -> Self {
+        let mut result = Self {
+            h: [0; 8],
+            ml: 0,
+            buffer: vec![],
+        };
+        result.reset();
+        return result;
+    }
+}
+
+impl Sha256 {
+    fn compress(&mut self, chunk: &[u8]) {
+        let mut w = to_w32_be(chunk);
+        w.resize(64, 0);
+        for i in 16..64 {
+            let s0 = w[i - 15].rotate_right(7) ^ w[i - 15].rotate_right(18) ^ (w[i - 15] >> 3);
+            let s1 = w[i - 2].rotate_right(17) ^ w[i - 2].rotate_right(19) ^ (w[i - 2] >> 10);
+            w[i] = w[i - 16]
+                .overflowing_add(s0)
+                .0
+                .overflowing_add(w[i - 7])
+                .0
+                .overflowing_add(s1)
+                .0;
+        }
+        let w = w;
+        let [mut a, mut b, mut c, mut d, mut e, mut f, mut g, mut h] = self.h;
+
+        #[allow(non_snake_case)]
+        for i in 0..64 {
+            let S1 = (e.rotate_right(6)) ^ (e.rotate_right(11)) ^ (e.rotate_right(25));
+            let ch = (e & f) ^ ((!e) & g);
+            let temp1 = h
+                .overflowing_add(S1)
+                .0
+                .overflowing_add(ch)
+                .0
+                .overflowing_add(SHA1_K[i])
+                .0
+                .overflowing_add(w[i])
+                .0;
+            let S0 = a.rotate_right(2) ^ a.rotate_right(13) ^ a.rotate_right(22);
+            let maj = (a & b) ^ (a & c) ^ (b & c);
+            let temp2 = S0.overflowing_add(maj).0;
+
+            h = g;
+            g = f;
+            f = e;
+            e = d.overflowing_add(temp1).0;
+            d = c;
+            c = b;
+            b = a;
+            a = temp1.overflowing_add(temp2).0;
+        }
+
+        self.h[0] = self.h[0].overflowing_add(a).0;
+        self.h[1] = self.h[1].overflowing_add(b).0;
+        self.h[2] = self.h[2].overflowing_add(c).0;
+        self.h[3] = self.h[3].overflowing_add(d).0;
+        self.h[4] = self.h[4].overflowing_add(e).0;
+        self.h[5] = self.h[5].overflowing_add(f).0;
+        self.h[6] = self.h[6].overflowing_add(g).0;
+        self.h[7] = self.h[7].overflowing_add(h).0;
+    }
+}
+
+const SHA1_K: [u32; 64] = [
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+];
+
+impl Digest for Sha256 {
+    fn reset(&mut self) {
+        self.h[0] = 0x6a09e667;
+        self.h[1] = 0xbb67ae85;
+        self.h[2] = 0x3c6ef372;
+        self.h[3] = 0xa54ff53a;
+        self.h[4] = 0x510e527f;
+        self.h[5] = 0x9b05688c;
+        self.h[6] = 0x1f83d9ab;
+        self.h[7] = 0x5be0cd19;
+        self.ml = 0;
+        self.buffer.clear();
+    }
+
+    fn update(&mut self, input: &[u8]) {
+        self.ml += (input.len() as u64) * 8u64;
+        let mut merged = self.buffer.clone();
+        merged.extend(input);
+        for chunk in merged.chunks(Self::block_size()) {
+            if chunk.len() != Self::block_size() {
+                self.buffer.resize(chunk.len(), 0);
+                self.buffer.copy_from_slice(chunk);
+                return;
+            }
+            self.compress(chunk);
+        }
+    }
+
+    fn digest(&mut self) -> Vec<u8> {
+        // add a trailing 1
+        self.buffer.extend_from_slice(&[0x80]);
+        let mut padding_needed = 56i32 - (self.buffer.len() % Self::block_size()) as i32;
+        if padding_needed < 0 {
+            padding_needed += Self::block_size() as i32;
+        }
+        self.buffer
+            .extend(std::iter::repeat(0).take(padding_needed as usize));
+        self.buffer.extend_from_slice(&self.ml.to_be_bytes());
+        assert_eq!(0, self.buffer.len() % Self::block_size());
+        self.buffer
+            .clone()
+            .chunks_exact(Self::block_size())
+            .for_each(|c| self.compress(c));
+
+        let mut hh = vec![];
+        hh.extend_from_slice(&self.h[0].to_be_bytes());
+        hh.extend_from_slice(&self.h[1].to_be_bytes());
+        hh.extend_from_slice(&self.h[2].to_be_bytes());
+        hh.extend_from_slice(&self.h[3].to_be_bytes());
+        hh.extend_from_slice(&self.h[4].to_be_bytes());
+        hh.extend_from_slice(&self.h[5].to_be_bytes());
+        hh.extend_from_slice(&self.h[6].to_be_bytes());
+        hh.extend_from_slice(&self.h[7].to_be_bytes());
+
+        self.reset();
+        hh
+    }
+
+    fn digest_size() -> usize {
+        return 32;
+    }
+
+    fn block_size() -> usize {
+        64
+    }
+}
+
+#[derive(Clone)]
 pub struct PrefixMac<T: Digest> {
     key: Vec<u8>,
     digest: T,
@@ -502,6 +655,29 @@ mod tests {
         }
 
         let mut hash = Sha1::default();
+        for test in &vectors {
+            hash.update(test.0);
+            assert_eq!(test.1, to_hex(hash.digest()));
+        }
+    }
+
+    #[test]
+    fn sha256_kats() {
+        let vectors = vec![
+            ("".as_bytes(), "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"),
+            ("abc".as_bytes(), "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"),
+            ("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq".as_bytes(), "248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1"),
+            ("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu".as_bytes(), "cf5b16a778af8380036ce59e7b0492370b249b11e8f07a51afac45037afee9d1"),
+            ("abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstuabcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstuabcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu".as_bytes(), "b584a05e1af03e9e2201550df419266f1a18993eb8999fa98bda4a140da36a66"),
+
+        ];
+        for test in &vectors {
+            let mut hash = Sha256::default();
+            hash.update(test.0);
+            assert_eq!(test.1, to_hex(hash.digest()));
+        }
+
+        let mut hash = Sha256::default();
         for test in &vectors {
             hash.update(test.0);
             assert_eq!(test.1, to_hex(hash.digest()));
