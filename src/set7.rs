00186614587151fc7c598bc68ad2d5e1a9ccd847
SalusaSecondus <salusa@salusa.dev~>
2024-07-06 22:39:12 -0700

    Challenge 50

--- src/set7.rs
+++ src/set7.rs
@@ -1,13 +1,16 @@
-
-
-
 #[cfg(test)]
 mod tests {
-    use crate::{oracles::Challenge49Oracle, padding::{self, Padding}, xor};
-    use anyhow::{Result, Context};
+    use crate::{
+        aes::AesKey,
+        oracles::Challenge49Oracle,
+        padding::{self, Padding},
+        xor,
+    };
+    use anyhow::{Context, Result};
+    use itertools::Itertools;
 
     #[test]
-    fn challenge40_1() -> Result<()> {
+    fn challenge49_1() -> Result<()> {
         let oracle: Challenge49Oracle = Default::default();
 
         let iv = [0u8; 16];
@@ -22,45 +25,85 @@ mod tests {
         let mut iv2 = iv;
         iv2[5] = diff;
         println!("Hacked");
-        assert!(oracle.verify1(&iv2 , &tag1, "9", "7", 1000000).is_ok());
+        assert!(oracle.verify1(&iv2, &tag1, "9", "7", 1000000).is_ok());
         Ok(())
     }
 
     #[test]
-    fn challenge40_2() -> Result<()> {
+    fn challenge49_2() -> Result<()> {
         let oracle: Challenge49Oracle = Default::default();
         let pkcs7 = Padding::Pkcs7Padding(16);
 
-        let txns1 = vec![
-            ("0", 1),
-            ("15", 32),
-            ("2",45)
-        ];
+        let txns1 = vec![("0", 1), ("15", 32), ("2", 45)];
         let good_mac = oracle.sign2("1", &txns1)?;
         println!("Good mac: {}", hex::encode(&good_mac));
         let good_signed = pkcs7.pad(Challenge49Oracle::sts2("1", &txns1).as_bytes())?;
 
-        let txns2 = vec![
-            ("99", 1000000),
-            ("99", 10),
-            ("99", 1000000)
-        ];
+        let txns2 = vec![("99", 1000000), ("99", 10), ("99", 1000000)];
         println!("{}", Challenge49Oracle::sts2("99", &txns2));
         let evil_mac = oracle.sign2("99", &txns2)?;
         let evil_base = pkcs7.pad(Challenge49Oracle::sts2("99", &txns2).as_bytes())?;
-        
 
         let mut forged = good_signed.clone();
         let mut evil_chunks = evil_base.chunks_exact(16);
         let first_evil = evil_chunks.next().context("Too few chunks")?; // Drop first chunk
-        // let glue_chunk = evil_chunks.next().context("Too few chunks")?;
-        // let xored = glue_chunk.to_owned();
+                                                                        // let glue_chunk = evil_chunks.next().context("Too few chunks")?;
+                                                                        // let xored = glue_chunk.to_owned();
         let xored = xor(first_evil, &good_mac);
         forged.extend_from_slice(&xored);
         evil_chunks.for_each(|c| forged.extend_from_slice(c));
         println!("{}", hex::encode(&forged));
         let forged = pkcs7.unpad(&forged)?;
         assert!(oracle.verify_mac(&[0u8; 16], &forged, &evil_mac));
+        Ok(())
+    }
+
+    #[test]
+    fn challenge50() -> Result<()> {
+        let iv = &[0u8; 16];
+        let pkcs7 = Padding::Pkcs7Padding(16);
+        let key = AesKey::new(b"YELLOW SUBMARINE")?;
+        let good_js = "alert('MZA who was that?');\n";
+        let good_hash = "296b8d7cb78a243dda4d0a61d33bbdd1";
+        assert_eq!(
+            good_hash,
+            hex::encode(key.cbc_mac(iv, good_js.as_bytes())?)
+        );
+        let padded_good_js = pkcs7.pad(good_js.as_bytes())?;
+        println!("{}", hex::encode(&padded_good_js));
+        let encrypted_good = key.encrypt_cbc(&[0u8; 16], &padded_good_js)?;
+        println!("Encrypted good: {}", hex::encode(&encrypted_good));
+
+        let decrypted_tag = key.decrypt_block(encrypted_good.chunks_exact(16).last().context("To few")?);
+        let expected_end_pt = xor(encrypted_good.chunks_exact(16).tail(2).next().context("too few")?, &decrypted_tag);
+        println!("{}", hex::encode(&expected_end_pt));
+
+        let target_pt = "alert('Ayo, the Wu is back!');\n <!--                            -->";
+        let padded_target = pkcs7.pad(target_pt.as_bytes())?;
+        println!("Padded target: {}", hex::encode(&padded_target));
+        let final_target = padded_target.chunks_exact(16).last().unwrap();
+        let encrypted_target = key.encrypt_cbc(iv, &padded_target)?;
+
+        let mut replaced_end = vec![];
+        replaced_end.extend_from_slice(&encrypted_target[..encrypted_target.len()-16]);
+        replaced_end.extend_from_slice(&hex::decode(good_hash)?);
+        println!("encrypted_target: {}", hex::encode(&encrypted_target));
+        println!("replaced_end:     {}", hex::encode(&replaced_end));
+        let tmp_len = replaced_end.len();
+
+        for idx in 0..16 {
+            replaced_end[tmp_len - 32 + idx] = 0;
+        }
+        let decrypted_replaced_end = key.decrypt_cbc(iv, &replaced_end)?;
+        println!("decrypted_target: {}", hex::encode(&decrypted_replaced_end));
+        let final_diff = xor(final_target, &decrypted_replaced_end.chunks_exact(16).last().unwrap());
+        println!("final_diff: {}", hex::encode(&final_diff));
+        for (idx, val) in final_diff.iter().enumerate() {
+            replaced_end[tmp_len - 32 + idx] = *val;
+        }
+        println!("replaced_end:     {}", hex::encode(&replaced_end));
+        let decrypted_replaced_end = key.decrypt_cbc(iv, &replaced_end)?;
+        println!("decrypted_target: {}", hex::encode(&decrypted_replaced_end));
         Ok(())
     }
 }
