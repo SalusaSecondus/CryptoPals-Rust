669850419abc56a8616b6686dd52baa028f350d9
SalusaSecondus <salusa@salusa.dev~>
2024-07-08 15:03:24 -0700

    Challenge 52

--- src/set7.rs
+++ src/set7.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fmt::Display, iter, slice::Iter, sync::mpsc::channel};
+use std::{collections::HashMap, fmt::Display, sync::mpsc::channel};
 
 use hex::ToHex;
 
@@ -26,7 +26,7 @@ impl RD0 {
         key.resize(16, 0);
         let aes = AesKey::new(&key).unwrap();
         let mut long_state = aes.encrypt_block(block);
-        long_state.resize(2, 0);
+        long_state.resize(Self::digest_size(), 0);
         long_state
         // vec![0u8; 2]
     }
@@ -76,6 +76,110 @@ impl Digest for RD0 {
 impl Display for RD0 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.state.encode_hex::<String>())
+    }
+}
+
+
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+pub struct RD1 {
+    initial: [u8; 4],
+    state: [u8; 4],
+    buffer: Option<Vec<u8>>,
+}
+
+impl RD1 {
+    fn compress(old_state: &[u8], block: &[u8]) -> Vec<u8> {
+        let mut key = vec![];
+        key.extend_from_slice(old_state);
+        key.resize(16, 0);
+        let aes = AesKey::new(&key).unwrap();
+        let mut long_state = aes.encrypt_block(block);
+        long_state.resize(Self::digest_size(), 0);
+        long_state
+        // vec![0u8; 2]
+    }
+}
+
+impl Digest for RD1 {
+    fn reset(&mut self) {
+        self.state = self.initial;
+    }
+
+    fn update(&mut self, input: &[u8]) {
+        let mut merged = self.buffer.take().unwrap_or_default();
+        merged.extend(input);
+        for block in merged.chunks(Self::block_size()) {
+            if block.len() != Self::block_size() {
+                self.buffer = Some(block.to_owned());
+            } else {
+                let new_state = Self::compress(&self.state, block);
+                self.state.copy_from_slice(&new_state);
+            }
+        }
+    }
+
+    fn digest(&mut self) -> Vec<u8> {
+        let remaining = self.buffer.take().unwrap_or_default();
+        let padded = Padding::Pkcs7Padding(Self::block_size())
+            .pad(&remaining)
+            .unwrap();
+        let result = Self::compress(&self.state, &padded);
+        self.reset();
+        result
+    }
+
+    fn digest_size() -> usize {
+        4
+    }
+
+    fn block_size() -> usize {
+        16
+    }
+
+    fn oid() -> Option<&'static asn1::ObjectIdentifier<'static>> {
+        None
+    }
+}
+
+impl Display for RD1 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.state.encode_hex::<String>())
+    }
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq)]
+struct RD2 {
+    rd0: RD0,
+    rd1: RD1
+}
+
+impl Digest for RD2 {
+    fn reset(&mut self) {
+        self.rd0.reset();
+        self.rd1.reset();
+    }
+
+    fn update(&mut self, input: &[u8]) {
+        self.rd0.update(input);
+        self.rd1.update(input);
+    }
+
+    fn digest(&mut self) -> Vec<u8> {
+        let mut result = self.rd0.digest();
+        result.extend(self.rd1.digest());
+        result
+    }
+
+    fn digest_size() -> usize {
+        RD0::digest_size() + RD1::digest_size()
+    }
+
+    fn block_size() -> usize {
+        16
+    }
+
+    fn oid() -> Option<&'static asn1::ObjectIdentifier<'static>> {
+        None
     }
 }
 
@@ -137,16 +241,6 @@ where
         current_state.copy_from_slice(&collision.2);
         pairs.push(vec![collision.0, collision.1]);
     }
-
-    let tmp = pairs
-        .iter()
-        .map(|v| v.iter())
-        .multi_cartesian_product()
-        .map(|i| {
-            let mut result = i[0].clone();
-            result.extend_from_slice(&i[1]);
-            result
-        });
     Ok((pairs, count))
 }
 
@@ -189,7 +283,7 @@ fn pairs_to_product(pairs: &[Vec<Vec<u8>>]) -> impl Iterator<Item = Vec<u8>> + '
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashSet;
+    use std::{collections::HashSet, hash::Hash};
 
     use crate::{
         aes::AesKey,
@@ -475,6 +569,53 @@ mod tests {
     }
 
     #[test]
+    fn challenge52_smoke2() -> Result<()> {
+        let mut dgst = RD1::default();
+
+        dgst.update(b"Greg");
+        println!("rd0(\"Greg\") = {}", dgst.digest().encode_hex::<String>());
+        dgst.update(b"Sarah");
+        println!("rd0(\"Sarah\") = {}", dgst.digest().encode_hex::<String>());
+        dgst.update(b"Greg");
+        println!("rd0(\"Greg\") = {}", dgst.digest().encode_hex::<String>());
+        dgst.update(b"Sarah");
+        println!("rd0(\"Sarah\") = {}", dgst.digest().encode_hex::<String>());
+
+        let collisions = find_collision(RD1::block_size(), &[0u8; 4], RD1::compress)?;
+
+        dgst.update(&collisions.0);
+        println!(
+            "rd1({}) = {}",
+            &collisions.0.encode_hex::<String>(),
+            dgst.digest().encode_hex::<String>()
+        );
+        dgst.update(&collisions.1);
+        println!(
+            "rd1({}) = {}",
+            &collisions.1.encode_hex::<String>(),
+            dgst.digest().encode_hex::<String>()
+        );
+        println!("Took {} compressions!", collisions.3);
+        Ok(())
+    }
+
+#[test]
+    fn challenge52_smoke3() -> Result<()> {
+        let mut dgst = RD2::default();
+
+        dgst.update(b"Greg");
+        println!("rd0(\"Greg\") = {}", dgst.digest().encode_hex::<String>());
+        dgst.update(b"Sarah");
+        println!("rd0(\"Sarah\") = {}", dgst.digest().encode_hex::<String>());
+        dgst.update(b"Greg");
+        println!("rd0(\"Greg\") = {}", dgst.digest().encode_hex::<String>());
+        dgst.update(b"Sarah");
+        println!("rd0(\"Sarah\") = {}", dgst.digest().encode_hex::<String>());
+
+        Ok(())
+    }
+
+    #[test]
     fn challenge52_1() -> Result<()> {
         let mut dgst = RD0::default();
         let collisions = find_2n_collisions(10, RD0::block_size(), &[0u8; 2], RD0::compress)?;
@@ -489,6 +630,37 @@ mod tests {
             assert!(seen.insert(c));
         }
         println!("Took {} compressions!", collisions.1);
+        Ok(())
+    }
+
+    #[test]
+    fn challenge52_2() -> Result<()> {
+        let collisions = find_2n_collisions(20, RD0::block_size(), &[0u8; 2], RD0::compress)?;
+        let mut seen : HashMap<Vec<u8>, Vec<u8>> = HashMap::new();
+        println!("Took {} compressions!", collisions.1);
+        let mut compressions = collisions.1;
+
+        let mut rd1 = RD1::default();
+        for c in pairs_to_product(&collisions.0) {
+            rd1.update(&c);
+            let dgst = rd1.digest();
+            compressions += 1;
+            if let Some(other) = seen.get(&dgst) {
+                let mut rd2 = RD2::default();
+                rd2.update(other);
+                let dgst1 = rd2.digest();
+                rd2.update(&c);
+                let dgst2 = rd2.digest();
+                println!("rd2({}) -> {}", other.encode_hex::<String>(), dgst1.encode_hex::<String>());
+                println!("rd2({}) -> {}", c.encode_hex::<String>(), dgst2.encode_hex::<String>());
+                println!("Took {} compressions!", compressions);
+                assert_ne!(other, &c);
+                assert_eq!(dgst1, dgst2);
+                break;
+            } else {
+                seen.insert(dgst, c);
+            }
+        }
         Ok(())
     }
 }
